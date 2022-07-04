6ff1c1c9bb7a9f2d3c55d8418c2a1f0119d5a59f
sabrina michee <martmichee@gmail.com>
2022-07-04 11:33:18 +0200

    3 syntaxes différentes pour designer une fonction ( conventionnelle, ternaire, operator)

--- includes/main.php
+++ includes/main.php
@@ -3,12 +3,18 @@
 <h1>MAIN</h1>
 <?php
 // isset verifie si la page existe
+// syntaxe conventionnelle
 if(isset($_get['page'])){
     $page = $_get['page'];
     dump($page);
 }else{
     $page = ['accueil'];
 }
+// syntaxe ternaire
+// $page = isset($_GET['page']) ? $_GET['page'] : "accueil"; 
+
+// Null coalescent operator (php8 surtout pour les booleens)
+// $page = $_GET['page'] ?? "accueil";
 ?>
 
 
